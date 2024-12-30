1316826234da0360910e4d5f44a5f19c54084568
Neo Yin <yinzhang@berkeley.edu>
2024-12-29 16:27:41 -0800

    testing out end points

--- yolo.py
+++ yolo.py
@@ -89,6 +89,6 @@ def detect():
 
 
 if __name__ == "__main__":
-    # app.run(host="0.0.0.0", port=9999, debug=True)
+    app.run(host="0.0.0.0", port=9999, debug=True)
 
-    image_path = "3316.jpg"
+    # image_path = "3316.jpg"
